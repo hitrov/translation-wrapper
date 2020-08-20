de9f53c98c0145a73cfb17a7b0cb0e8ec574a24f
Alexander Hitrov <p998hih@fspa.myntet.se>
2020-08-20 18:03:29 +0300

    state, counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,106 @@
-import React, { useEffect, useState } from 'react';
+import React, { MouseEventHandler, useEffect, useState } from 'react';
 import './App.css';
 
-const Hello = ({ header, content }: { header: string, content: string }) => (
+interface HelloProps {
+  header: string;
+  content: string;
+}
+
+const Hello = ({ header, content }: HelloProps) => (
   <div>
     <h1>{header}</h1>
     <p>{content}</p>
   </div>
 );
 
+interface ActionButtonProps {
+  text: string;
+  onClick: () => void;
+}
+
+const ActionButton = ({ onClick, text }: ActionButtonProps) => (
+  <button onClick={onClick}>{text}</button>
+);
+
 interface WithLoadingProps {
   loading: boolean;
+
   getHeader(): string;
   getContent(): string;
 }
 
-function withLoading<P extends object>(Component: React.ComponentType<P>) {
-  return class WithLoading extends React.Component<P & WithLoadingProps> {
-    render() {
-      const { loading, getContent, getHeader, ...props } = this.props;
+interface WithLoadingState {
+  header: string;
+  content: string;
+  counter: number;
+}
 
-      const content = getContent();
-      const header = getHeader();
+function withLoading<P extends object>(Component: React.ComponentType<P>) {
+  return class WithLoading extends React.Component<P & WithLoadingProps, WithLoadingState> {
+    state: Readonly<WithLoadingState> = {
+      header: '',
+      content: '',
+      counter: 0,
+    };
+
+    constructor(props: P & WithLoadingProps) {
+      super(props);
+
+      this.onClick = this.onClick.bind(this);
+      this.onAddClick = this.onAddClick.bind(this);
+    }
+
+    componentDidMount() {
+      const { getContent, getHeader } = this.props;
+
+      this.setState({
+        header: getHeader(),
+        content: getContent(),
+      });
+    }
+
+    onAddClick() {
+      this.setState({
+        counter: this.state.counter + 1,
+      })
+    }
+
+    onClick() {
+      console.log('onClick');
+      // debugger;
+      this.setState({
+        header: 'Теперь...',
+        content: '...по-русски!',
+      });
+    };
+
+    render() {
+      const { loading, ...props } = this.props;
+      // const { loading, getContent, getHeader, ...props } = this.props;
+      // const [ content, setContent ] = useState(getContent());
+      // const [ header, setHeader ] = useState(getHeader());
+      //
+      // const onClick = () => {
+      //   setHeader('Теперь...');
+      //   setContent('...по-русски!');
+      // };
 
       return (
         <>
           {loading && <div>loading...</div>}
           <Component
-            content={content}
-            header={header}
+            header={this.state.header}
+            content={this.state.content}
             {...props as P}
           />
+          <ActionButton
+            text={'Translate'}
+            onClick={this.onClick}
+          />
+          <div>
+            {this.state.counter}
+            <button onClick={this.onAddClick}>Add</button>
+          </div>
         </>
       );
     }
@@ -42,11 +113,11 @@ function App() {
   const [ loading, setLoading ] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 5000);
+    setTimeout(() => setLoading(false), 2000);
   });
 
-  const header = 'hello!';
-  const content = 'and welcome';
+  const header = 'Hello!..';
+  const content = '...and welcome';
 
   return (
     <div className="App">
@@ -57,6 +128,11 @@ function App() {
         getHeader={() => header}
         loading={loading}
       />
+
+      {/*<Hello*/}
+      {/*  header={header}*/}
+      {/*  content={content}*/}
+      {/*/>*/}
     </div>
   );
 }
