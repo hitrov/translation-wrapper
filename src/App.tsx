a628ced6251972fd138dacc37b843a242b20612b
Alexander Hitrov <p998hih@fspa.myntet.se>
2020-08-20 19:37:01 +0300

    fc

--- src/App.tsx
+++ src/App.tsx
@@ -7,48 +7,48 @@ interface HelloProps {
   TranslateButton?: React.ReactElement;
 }
 
-class Hello extends React.PureComponent<HelloProps> {
-// class Hello extends React.Component<HelloProps> {
-  componentDidUpdate(prevProps: Readonly<HelloProps>) {
-    console.log('componentDidUpdate', this.constructor.name);
-    console.log('prevProps', prevProps, 'this.props', this.props);
-  }
-
-  render() {
-    console.log('render Hello');
-
-    const { header, content, TranslateButton } = this.props;
-
-    return (
-      <div>
-        <h1>{header}</h1>
-        <p>{content}</p>
-        <div>
-          {TranslateButton}
-        </div>
-      </div>
-    );
-  }
-}
-
-// function Hello(props: HelloProps) {
-//   const { header, content, TranslateButton } = props;
+// class Hello extends React.PureComponent<HelloProps> {
+// // class Hello extends React.Component<HelloProps> {
+//   componentDidUpdate(prevProps: Readonly<HelloProps>) {
+//     console.log('componentDidUpdate', this.constructor.name);
+//     console.log('prevProps', prevProps, 'this.props', this.props);
+//   }
 //
-//   console.log('render Hello', {
-//     header,
-//     content,
-//   });
+//   render() {
+//     console.log('render Hello');
 //
-//   return (
-//     <div>
-//       <h1>{header}</h1>
-//       <p>{content}</p>
+//     const { header, content, TranslateButton } = this.props;
+//
+//     return (
 //       <div>
-//         {TranslateButton}
+//         <h1>{header}</h1>
+//         <p>{content}</p>
+//         <div>
+//           {TranslateButton}
+//         </div>
 //       </div>
-//     </div>
-//   );
+//     );
+//   }
 // }
+
+function Hello(props: HelloProps) {
+  const { header, content, TranslateButton } = props;
+
+  console.log('render Hello', {
+    header,
+    content,
+  });
+
+  return (
+    <div>
+      <h1>{header}</h1>
+      <p>{content}</p>
+      <div>
+        {TranslateButton}
+      </div>
+    </div>
+  );
+}
 
 interface WithLoadingProps {
   loading: boolean;
@@ -57,95 +57,56 @@ interface WithLoadingProps {
   getContent(): string;
 }
 
-interface WithLoadingState {
-  header: string;
-  content: string;
-  counter: number;
-}
-
 const withLoading = <P extends object>(Component: React.ComponentType<P>) =>
-  // class WithLoading extends React.PureComponent<P & WithLoadingProps, WithLoadingState> {
-  class WithLoading extends React.Component<P & WithLoadingProps, WithLoadingState> {
-    state: Readonly<WithLoadingState> = {
-      header: '',
-      content: '',
-      counter: 0,
-    };
+  (props: P & WithLoadingProps) => {
 
-    // constructor(props: P & WithLoadingProps) {
-    //   super(props);
-    //
-    //   // this.onClick = this.onClick.bind(this);
-    //   // this.onAddClick = this.onAddClick.bind(this);
-    // }
+    const { loading, getContent, getHeader, ...rest } = props;
+    const [ header, setHeader ] = useState(getHeader());
+    const [ content, setContent ] = useState(getContent());
+    const [ counter, setCounter ] = useState(0);
 
-    componentDidMount() {
-      const { getContent, getHeader } = this.props;
-
-      this.setState({
-        header: getHeader(),
-        content: getContent(),
-      });
+    const onAddClick = () => {
+      setCounter(counter + 1);
     }
 
-    componentDidUpdate(prevProps: Readonly<P & WithLoadingProps>, prevState: Readonly<WithLoadingState>) {
-      console.log('componentDidUpdate', this.constructor.name);
-      console.log('prevProps', prevProps, 'this.props', this.props);
-      console.log('prevState', prevState, 'this.state', this.state);
-    }
-
-    onAddClick = () => {
-      this.setState({
-        counter: this.state.counter + 1,
-      })
-    }
-
-    onClick = () => {
+    const onClick = () => {
       console.log('onClick');
       // debugger;
-      this.setState({
-        header: 'Теперь...',
-        content: '...по-русски!',
-      });
+      setHeader('Теперь...');
+      setContent('...по-русски!');
     };
 
-    render() {
-      const { loading, ...props } = this.props;
+    const TranslateButton = (
+      <ActionButton
+        text={'Translate'}
+        onClick={onClick}
+      />
+    );
 
-      const TranslateButton = (
-        <ActionButton
-          text={'Translate'}
-          onClick={this.onClick}
+    console.log('render WithLoading', {
+      header,
+      content,
+    });
+
+    return (
+      <>
+        <div>header: {header}</div>
+        <div>content: {content}</div>
+
+        {loading && <div>loading...</div>}
+        <Component
+          header={header}
+          content={content}
+          TranslateButton={TranslateButton}
+          {...rest as P}
         />
-      );
 
-      const { counter, header, content } = this.state;
-
-      console.log('render WithLoading', {
-        header,
-        content,
-      });
-
-      return (
-        <>
-          <div>header: {header}</div>
-          <div>content: {content}</div>
-
-          {loading && <div>loading...</div>}
-          <Component
-            header={header}
-            content={content}
-            TranslateButton={TranslateButton}
-            {...props as P}
-          />
-
-          <div>
-            {counter}
-            <button onClick={this.onAddClick}>Add</button>
-          </div>
-        </>
-      );
-    }
+        <div>
+          {counter}
+          <button onClick={onAddClick}>Add</button>
+        </div>
+      </>
+    );
   };
 
 const HelloLoading = withLoading(Hello);
@@ -155,13 +116,13 @@ function App() {
   const [ content, setContent ] = useState( 'Hello...');
   const [ header, setHeader ] = useState('...and welcome');
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-      setHeader('Now this is...');
-      setContent('...too late!');
-    }, 200000);
-  });
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //     setHeader('Now this is...');
+  //     setContent('...too late!');
+  //   }, 5000);
+  // });
 
   console.log('render App', {
     header,
@@ -198,12 +159,3 @@ const ActionButton = ({ onClick, text }: ActionButtonProps) => (
 );
 
 export default App;
-
-// const { loading, getContent, getHeader, ...props } = this.props;
-// const [ content, setContent ] = useState(getContent());
-// const [ header, setHeader ] = useState(getHeader());
-//
-// const onClick = () => {
-//   setHeader('Теперь...');
-//   setContent('...по-русски!');
-// };
